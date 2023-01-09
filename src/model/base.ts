e67469d0e5b3816d54d94170ae6dc7ac24d6b1dc
mensheng <feixiangmeng@outlook.com>
2023-01-09 18:13:37 +0800

    Remove log.

--- src/model/base.ts
+++ src/model/base.ts
@@ -42,7 +42,6 @@ export class BaseModel extends EventTarget implements IModel {
   }
 
   notifyUpdate(eventName: string, value: any[]) {
-    console.log(eventName, value);
     return true;
   }
 
