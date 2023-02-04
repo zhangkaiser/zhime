42bbdf1b976c809d24e16826a1ba9bafe3e7058d
mensheng <feixiangmeng@outlook.com>
2023-02-04 12:56:25 +0800

    Update librime submodule to latest.

--- src/api/common/files.ts
+++ src/api/common/files.ts
@@ -2,3 +2,12 @@
 export function isDir(file: File) {
   return !!file.webkitRelativePath;
 }
+
+export function getFileList(entries: string[]) {
+  let fetchingFiles = entries.map((entry) => fetch(entry).then(res => res.blob()));
+  return Promise.all(fetchingFiles).then(
+    blobs => blobs.map(
+      (blob, i) => new File([blob], entries[i].split("/").pop()!)
+    )
+  );
+}
