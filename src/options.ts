96ded49a75e3603bcfb5665d222be8aef06fa8ef
mensheng <feixiangmeng@outlook.com>
2023-02-13 23:52:53 +0800

    Update options.ts.

--- src/options.ts
+++ src/options.ts
@@ -1,9 +1,12 @@
 
 import { LitElement, html, css, render } from "lit";
 import { customElement, query, queryAll, state } from "lit/decorators.js";
+
 import shuangpinIcon from "./icons/chromeos-shuangpin-ime";
 import rimeIcon from "./icons/rime";
 
+import type { OptionsPage as LibrimeOptionsPage } from "../librime/emscripten/src/options-page";
+import { webDecoders } from "./consts/env";
 
 interface DecoderInfo {
   name: string,
@@ -42,8 +45,36 @@ const decoders: DecoderInfo[] = [
   }
 ]
 
-@customElement("options-page")
+const optionsInfo = [
+  {
+    description: "点击`部署方案/自定义部署方案/修改方案`后，需点击`保存并更新页面`按钮 方案才会部署成功。",
+  },
+  {
+    description: "内置了`rime输入法`解析器（支持选择内置方案/自定义方案/修改用户词库）。",
+  },
+  {
+    description: "rime解析器同时部署多个内置输入法方案，在输入框中使用快捷键（Ctrl + `）进行切换方案（例：简/繁体切换，中/英文切换，标点符号切换）。",
+  },
+  {
+    description: "使用扩展解析器时可以使用多种方案。",
+  }
+];
+
+const modes = [
+  {
+    name: "内置解析器"
+  },
+  {
+    name: "本地扩展解析器"
+  }
+];
+
+@customElement("zhime-options")
 class OptionsPage extends LitElement {
+
+  constructor() {
+    super();
+  }
   static styles = css`
     :host {
       font-size: 125%;
@@ -55,7 +86,7 @@ class OptionsPage extends LitElement {
       align-items: center;
     }
     .item-box {
-      width: 75vw;
+      width: 85%;
       border: 2px solid #000000;
       padding:7px 14px;
     }
@@ -96,12 +127,47 @@ class OptionsPage extends LitElement {
       line-height: 28px;
       margin-right: 14px;
     }
+
+    .active {
+      border-width: 2px 2px 0 2px;
+      border-style: solid;
+      z-index: 1024;
+      font-weight: bold;
+      border-radius: 2px;
+      background: #ebebeb;
+    }
+
+    .modes {
+      display: flex;
+    }
+
+    .mode-item {
+      padding: 24px 32px;
+      cursor: pointer;
+    }
+
+    .mode-container {
+      width: 85vw;
+      height: 60vh;
+      overflow: scroll;
+      padding: 7px 14px;
+      background: #ebebeb;
+      border: 2px solid black;
+      margin: -2px;
+      z-index: 1023;
+    }
+    
+    .info-bg {
+      width: 85vw;
+    }
     
   `;
 
   get mainElement() {
     return this.shadowRoot!.getElementById("maindecoder") as HTMLInputElement;
   }
+
+  @state() currentMode = 0;
 
   success() {
     let { value } = this.mainElement;
@@ -120,44 +186,110 @@ class OptionsPage extends LitElement {
     let { id } = target;
   }
 
-  render() {
+  chooseMode(e: Event) {
+    let target = e.currentTarget as HTMLDivElement;
+    let index = + (target.dataset.index ?? 0);
+    if (this.currentMode === index) return;
+    this.currentMode = index;
+  }
+
+  @query(".mode-container") modeContainer!: HTMLDivElement;
+
+  builtinDecoderOptionsUI() {
+    import("../librime/emscripten/src/options-page").then(async () => {
+      let librimeOptionsPage = document.createElement("options-page") as LibrimeOptionsPage;
+      let worker = new Worker(webDecoders.librime.scripts);
+      librimeOptionsPage.setWorker(worker);
+      let container = this.shadowRoot!.getElementById("options-container") as HTMLDivElement;
+      container.appendChild(librimeOptionsPage);
+    });
+    return html`<div id="options-container"></div>`;
+  }
+
+  extDecoderOptionsUI() {
     return html`
-      <div class="full-box">
-        <div class="input-box">
-          <span>主解析器扩展ID</span>
-          <input id="maindecoder" placeholder="在此输入解析器的扩展ID">
-          <div class="">
-            <button @click=${this.success}>保存</button>
+      <div class="input-box">
+        <span>主解析器扩展ID</span>
+        <input id="maindecoder" placeholder="在此输入解析器的扩展ID">
+        <div class="">
+          <button @click=${this.success}>保存</button>
+        </div>
+      </div>
+      <div class="line-title">
+        <span><h1>当前支持的解析器</h1></span>
+      </div>
+      ${decoders.map((decoderInfo) => html`
+        <div class="item-box">
+          <div class="name">
+            <h3>${decoderInfo.name}</h3>
+            <h3 class="author">${decoderInfo.author}</h3>
+          </div>
+          <div class="desc">
+            <span>${decoderInfo.description}</span>
+          </div>
+          <div class="download" .id=${decoderInfo.id} @click=${this.onClickDownloadBtn} >
+            <div class="download-btn" ?hidden=${!Reflect.has(decoderInfo, "github")}>
+              <a .href=${decoderInfo.github}>GitHub</a>
+            </div>
+            <div class="download-btn" ?hidden=${!Reflect.has(decoderInfo, "download")}>
+              <a .href=${decoderInfo.download}>直接下载</a>
+            </div>
+            <div class="download-btn" ?hidden=${!Reflect.has(decoderInfo, "“store")}
+              <a .href=${decoderInfo.store}>Google Web Store安装</a>
+            </div>
+
           </div>
         </div>
-        
-        <div class="line-title">
-          <span><h1>当前支持的解析器<h1></span>
-        </div>
-        ${decoders.map((decoderInfo) => html`
-          <div class="item-box">
-            <div class="name">
-              <h3>${decoderInfo.name}</h3>
-              <h3 class="author">${decoderInfo.author}</h3>
-            </div>
-            <div class="desc">
-              <span>${decoderInfo.description}</span>
-            </div>
-            <div class="download" .id=${decoderInfo.id} @click=${this.onClickDownloadBtn} >
-              <div class="download-btn" ?hidden=${!Reflect.has(decoderInfo, "github")}>
-                <a .href=${decoderInfo.github}>GitHub</a>
-              </div>
-              <div class="download-btn" ?hidden=${!Reflect.has(decoderInfo, "download")}>
-                <a .href=${decoderInfo.download}>直接下载</a>
-              </div>
-              <div class="download-btn" ?hidden=${!Reflect.has(decoderInfo, "“store")}
-                <a .href=${decoderInfo.store}>Google Web Store安装</a>
-              </div>
+      `)}
+    `;
+  }
 
+  getModeContainerUI() {
+
+    return html`
+      ${this.currentMode === 0 ? this.builtinDecoderOptionsUI() : this.extDecoderOptionsUI()}
+    `;
+  }
+
+  connectionModeUI() {
+    return html`
+      <div class="modes">
+        ${modes.map((item, index) => html`
+          <div @click="${this.chooseMode}" class=${this.currentMode == index ? "mode-item active" : "mode-item"} data-index="${index}">
+            <span>${item.name}</span>
+          </div>
+        `)}
+      </div>
+      <div class="mode-container">
+        ${this.getModeContainerUI()}
+      </div>
+    `;
+  }
+
+  optionsInfoUI() {
+    return html`
+      <div class="info-bg">
+        <div class="line-title">
+          <span><h1>当前支持状态</h1></span>
+        </div>
+        ${optionsInfo.map((item, index) => html`
+          <div class="info-box">
+            <div class="desc">
+              <span><b>${index + 1}. </b> ${item.description}</span>
             </div>
           </div>
         `)}
       </div>
+    `;
+  }
+
+  render() {
+    return html`
+      <div class="full-box">
+        ${this.connectionModeUI()}
+        ${this.optionsInfoUI()}
+      </div>
+
     `;
   }
 
@@ -166,6 +298,6 @@ class OptionsPage extends LitElement {
 let container = document.getElementById("container");
 
 render(
-  html`"<options-page></options-page>"`,
+  html`"<zhime-options></zhime-options>"`,
   container!
 )
